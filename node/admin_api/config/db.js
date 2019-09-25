9730deae7c65a94ec801eba48e9f0179eeb3cb67
Jhoel Gonzalez <jhoelgonzalez@Jhoels-MacBook-Pro.local>
2019-09-25 17:02:46 -0600

    comment removal

--- node/admin_api/config/db.js
+++ node/admin_api/config/db.js
@@ -3,7 +3,6 @@ const { dbConfig } = require('../config');
 let db;
 
 if (!db) {
-  console.log(dbConfig);
   db = mysql.createConnection(dbConfig);
   db.connect();
   console.log("conected to db");
